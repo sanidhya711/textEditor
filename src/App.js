bf5ebf747a97bc1e4b1c15249e72bb1e0ae49214
sanidhyajain1 <sanidhyajain077@gmail.com>
2020-05-26 17:01:39 +0530

    r

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
 import './App.css';
-function hii(){
-  alert(
-    'Alert Title',
-    'My Alert Msg',
-    [
-      {
-        text: 'Ask me later',
-        onPress: () => console.log('Ask me later pressed')
-      },
-      {
-        text: 'Cancel',
-        onPress: () => console.log('Cancel Pressed'),
-        style: 'cancel'
-      },
-      { text: 'OK', onPress: () => console.log('OK Pressed') }
-    ],
 
 function App() {
   return (
     <div className="App">
- 
+ <h1>hiiii</h1>
     </div>
   );
 }
